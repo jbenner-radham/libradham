58413777610d3bf0795c7604c7436246473c1b9d
James Benner <james.benner@gmail.com>
2013-04-20 16:51:26 -0400

    Optimized control flow and made it DRY compliant.

--- rh_stra.c
+++ rh_stra.c
@@ -1,14 +1,21 @@
 extern char* rh_stra(const char* src)
 {
-    /* The char* returned from this function MUST be free'd 
-       by the programmer at a later point. */
+    // RadHam STRing Allocation
+    // ------------------------
+    // The char* returned from this function MUST be free'd 
+    // by the programmer at a later point.
     
-    if ((strlen(src) + 1) >= (BUFSIZ -1))
-        return NULL;
+    if ((strlen(src) + 1) < (BUFSIZ - 1)) {
+        char *dest = NULL;
+        size_t dest_s = sizeof(char) * (strlen(src) + 1);
+
+        if ((dest = (char*)malloc(dest_s))) {
+            strcpy(dest, src);
+            
+            return dest;
+        }
+
+    }
     
-    size_t dest_s = sizeof(char) * (strlen(src) + 1);
-    char *dest = (char*)malloc(dest_s);
-    strcpy(dest, src);
-    
-    return dest;
+    return NULL;
 }
