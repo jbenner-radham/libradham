be0f0208380fb02a74fe8b76f0948ef5efb28547
James Benner <james.benner@gmail.com>
2013-04-20 16:50:14 -0400

    Updated syntax and general cleanup.

--- radham.h
+++ radham.h
@@ -1,18 +1,10 @@
 #ifndef _LIBRADHAM_H
 #define _LIBRADHAM_H
 
-#ifndef _STDLIB_H
 #include <stdlib.h> // malloc
-#endif
-
-#ifndef _STDIO_H
 #include <stdio.h>  // size_t, BUFSIZ, NULL
-#endif
-
-#ifndef _STRING_H
-#include <string.h> // strlen
-#endif
+#include <string.h> // strlen, strcpy
 
 extern char* rh_stra(const char* src);
 
-#endif
+#endif //_LIBRADHAM_H
