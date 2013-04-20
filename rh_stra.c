dde883f6f677fc8f6abde8c15804f320cd906e89
James Benner <james.benner@gmail.com>
2013-04-20 17:00:16 -0400

    More DRY tweaks.

--- rh_stra.c
+++ rh_stra.c
@@ -4,10 +4,11 @@ extern char* rh_stra(const char* src)
     // ------------------------
     // The char* returned from this function MUST be free'd 
     // by the programmer at a later point.
-    
-    if ((strlen(src) + 1) < (BUFSIZ - 1)) {
+
+    size_t dest_s = sizeof(char) * (strlen(src) + 1);
+
+    if (dest_s < (BUFSIZ - 1)) {
         char *dest = NULL;
-        size_t dest_s = sizeof(char) * (strlen(src) + 1);
 
         if ((dest = (char*)malloc(dest_s))) {
             strcpy(dest, src);
