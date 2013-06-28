04fc341bd74146c94e0bd2834e8bc32d76b2e646
James Benner <james.benner@gmail.com>
2013-06-28 00:48:12 -0400

    Adding just in case anything is out of sync...

--- rh_stra.c
+++ rh_stra.c
@@ -5,6 +5,8 @@ extern char* rh_stra(const char* src)
     // The char* returned from this function MUST be free'd 
     // by the programmer at a later point.
 
+    // Char pointers don't need a +1 for the NUL byte... I think.
+
     size_t dest_s = sizeof(char) * (strlen(src) + 1);
 
     if (dest_s < (BUFSIZ - 1)) {
