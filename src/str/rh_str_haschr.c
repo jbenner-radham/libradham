be59ebbb3ffec45afb4f237d2b49732af4a132e1
jbenner55 <james.benner@gmail.com>
2013-07-04 22:57:07 -0400

    Fixed the function name... oops.

--- src/str/rh_str_haschr.c
+++ src/str/rh_str_haschr.c
@@ -1,5 +1,5 @@
 // @requires: rh_str_chrpos
-bool rh_str_chrhas(const char* str, char chr)
+bool rh_str_haschr(const char* str, char chr)
 {
 	return rh_str_chrpos(str, chr) != -1? true : false; 
 }
