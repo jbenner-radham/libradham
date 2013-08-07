be9edf510cec2db421e39bcc992dd69140afbcf5
jbenner55 <james.benner@gmail.com>
2013-08-06 23:22:12 -0400

    Update rh_str_startswith.c

--- src/str/rh_str_startswith.c
+++ src/str/rh_str_startswith.c
@@ -1,7 +1,7 @@
 bool rh_str_startswith(char* needle, char* haystack)
 {  
     for (size_t i = 0, needle_len = strlen(needle); i < needle_len; ++i)
-		if (needle[i] != haystack[i])
+        if (needle[i] != haystack[i])
             return false;
 
     return true;
@@ -26,9 +26,9 @@ int main()
 {
 
     char *haystack = "say cheese";
-	char say[] = "skay";
+    char say[] = "skay";
 
-	rh_str_startswith(say, haystack) ? puts("Yup") : puts("Nope");
+    rh_str_startswith(say, haystack) ? puts("Yup") : puts("Nope");
 
 }
 */
