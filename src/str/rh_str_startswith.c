f39a108d4947bf409510643032a2ba09e5afaffe
jbenner55 <james.benner@gmail.com>
2013-08-06 23:14:26 -0400

    Create rh_str_startswith.c

---
+++ src/str/rh_str_startswith.c
@@ -0,0 +1,32 @@
+bool rh_str_startswith(char* needle, char* haystack)
+{  
+	for (size_t i = 0, needle_len = strlen(needle); i < needle_len; ++i)
+		if (needle[i] != haystack[i])
+			return false;
+
+	return true;
+}
+
+/*
+// -------------------------------
+// Example of function stuff, yay!
+// -------------------------------
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool rh_str_startswith(char* needle, char* haystack);
+
+int main()
+{
+
+    char *haystack = "say cheese";
+	char say[] = "skay";
+
+	rh_str_startswith(say, haystack) ? puts("Yup") : puts("Nope");
+
+}
+*/
